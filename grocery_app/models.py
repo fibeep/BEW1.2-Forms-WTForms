b38c86159b48e6049ba7d2948d8af906de0b83f0
Salo <salomonco@gmail.com>
2021-02-15 19:09:43 +0100

    added relationships + bridge table

--- grocery_app/models.py
+++ grocery_app/models.py
@@ -4,6 +4,11 @@ from grocery_app import db
 from grocery_app.utils import FormEnum
 from flask_login import UserMixin
 
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
 class ItemCategory(FormEnum):
     """Categories of grocery items."""
     PRODUCE = 'Produce'
@@ -22,6 +27,14 @@ class GroceryStore(db.Model):
     created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     created_by = db.relationship('User')
 
+
+
+
+bridge_table = db.Table('user_item_table',
+                     db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('item_id', db.Integer, db.ForeignKey('grocery_item.id'))
+                     )
+
 class GroceryItem(db.Model):
     """Grocery Item model."""
     id = db.Column(db.Integer, primary_key=True)
@@ -29,13 +42,14 @@ class GroceryItem(db.Model):
     price = db.Column(db.Float(precision=2), nullable=False)
     category = db.Column(db.Enum(ItemCategory), default=ItemCategory.OTHER)
     photo_url = db.Column(URLType)
-    store_id = db.Column(
-        db.Integer, db.ForeignKey('grocery_store.id'), nullable=False)
+    store_id = db.Column(db.Integer, db.ForeignKey('grocery_store.id'), nullable=False)
     store = db.relationship('GroceryStore', back_populates='items')
     created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     created_by = db.relationship('User')
+    # users = db.relationship('User', secondary=bridge_table)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    shopping_list_items = db.relationship("GroceryItem", secondary=bridge_table)
